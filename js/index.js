ee2b77ae8f336d8dd4f05fc20686fb70ae1b965c
AgataS23 <agata.stoch@gmail.com>
2023-02-15 10:05:39 +0100

    improved and corected some features

--- js/index.js
+++ js/index.js
@@ -1,3 +1,7 @@
-import * as navigation from './navigation.js';
-import * as footer from './footer-year.js';
-import * as registration from './registration.js';
+import * as navigation from "./navigation.js";
+import * as footer from "./footer-year.js";
+import * as registration from "./registration.js";
+// import * as login from "./login.js";
+// import * as slider from "./slider.js";
+// import * as charts from "./profile-charts.js";
+// import * as transactions from "./profile-transactions.js";
--- js/registration.js
+++ js/registration.js
@@ -8,31 +8,29 @@ export const email2 = setQuerySelector("#email2");
 
 export const isValidate = form.addEventListener("submit", (e) => {
 	e.preventDefault();
+	let isUsernameValid = checkUsername();
+	let isPasswordValid = checkPassword();
+	let isEmailValid = checkEmail();
+	let isEmail2Valid = checkEmail2();
 
-	validateInputs();
+	let formValid =
+		isUsernameValid && isPasswordValid && isEmailValid && isEmail2Valid;
 
-	// if () {
-	fnSavedData();
-	const inputs = document.querySelectorAll("input");
-	inputs.forEach((input) => {
-		input.value = "";
-	});
-	// }
-	// if (validateInputs) {
-	// 	const inputs = document.querySelectorAll("input");
-	// 	inputs.forEach((input) => {
-	// 		input.value = "";
-	// 	});
-	// 	window.location.href = "profile.html";
-	// }
+	if (formValid) {
+		fnSavedData();
+		const inputs = document.querySelectorAll("input");
+		inputs.forEach((input) => {
+			input.value = "";
+			input.style.border = "2px solid var(--light2-color)";
+		});
+
+		// window.location.href = "profile.html";
+	}
 });
 
 export const setError = (element, message) => {
-	// const formControl = document.querySelector('.form-control');
-
 	const formControl = element.parentElement;
 	const errorDisplay = formControl.querySelector("small");
-
 	errorDisplay.innerText = message;
 	formControl.classList.add("error");
 	formControl.classList.remove("success");
@@ -41,15 +39,13 @@ export const setError = (element, message) => {
 export const setSuccess = (element) => {
 	const formControl = element.parentElement;
 	const errorDisplay = formControl.querySelector("small");
-
 	errorDisplay.innerText = "";
 	formControl.classList.add("success");
 	formControl.classList.remove("error");
 };
 
 export const isValidUsername = (username) => {
-	const str = /^[A-Za-z0-9 ]*$/;
-	return str.test(username);
+	return /^[A-Za-z0-9 ]*$/.test(username);
 };
 
 export const isValidEmail = (email) => {
@@ -58,83 +54,117 @@ export const isValidEmail = (email) => {
 	return regExEmail.test(String(email).toLowerCase());
 };
 
-export const validateInputs = () => {
-	const usernameValue = username.value.trim();
-	const passwordValue = password.value.trim();
-	const emailValue = email.value.trim();
-	const email2Value = email2.value.trim();
+export const isValidPassword = (password) => {
+	const re = new RegExp(
+		"^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[!@#$%\^&*])(?=.{8,})",
+	);
+	return re.test(password);
+};
 
-	if (!usernameValue) {
+export const isRequired = (value) => (value === "" ? false : true);
+export const isBetween = (length, min, max) =>
+	length < min || length > max ? false : true;
+
+export const checkUsername = () => {
+	const usernameValue = username.value.trim();
+	let valid = false;
+	const min = 6;
+	const max = 16;
+
+	if (!isRequired(usernameValue)) {
 		setError(username, "Nazwa użytkownika jest wymagana");
-	} else if (!isValidUsername(usernameValue)) {
+	} else if (!isBetween(usernameValue.length, min, max)) {
 		setError(
 			username,
-			"Nazwa użytkownika może składać się z liter i/lub liczb",
+			`Nazwa użytkownika musi zawierać od ${min} do ${max} znaków liter lub liczb`,
 		);
-	} else if (usernameValue.length < 6) {
-		setError(username, "Nazwa użytkownika musi mieć conajmniej 6 znaków");
-	} else if (usernameValue.length > 16) {
-		setError(username, "Nazwa użytkownika może mieć maksymalnie 16 znaków");
 	} else {
 		setSuccess(username);
+		valid = true;
 	}
+	return valid;
+};
 
-	if (passwordValue === "") {
+export const checkPassword = () => {
+	const passwordValue = password.value.trim();
+	let valid = false;
+	if (!isRequired(passwordValue)) {
 		setError(password, "Hasło jest wymagane");
-	} else if (passwordValue.length < 6) {
-		setError(password, "Hasło musi mieć conajmniej 6 znaków");
+	} else if (!isValidPassword(passwordValue)) {
+		setError(
+			password,
+			"Hasło musi mieć co najmniej 8 znaków, (1 małą literę, 1 wielką literę, 1 cyfrę i 1 znak specjalny: !@#$%^&*)",
+		);
 	} else {
 		setSuccess(password);
+		valid = true;
 	}
+	return valid;
+};
 
-	if (emailValue === "") {
-		setError(email, "Adres email jest wymagany");
-	} else if (!isValidEmail(emailValue)) {
-		setError(email, "Podaj poprawny adres email");
+export const checkEmail = () => {
+	const emailValue = email.value.trim();
+	let valid = false;
+	if (!isRequired(emailValue)) {
+		setError(email, "Adres e-mail jest wymagany");
+	} else if (!isValidEmail) {
+		setError(email, "Podaj poprawny adres e-mail");
 	} else {
 		setSuccess(email);
+		valid = true;
 	}
+	return valid;
+};
 
-	if (email2Value === "") {
-		setError(email2, "Potwierdź adres email");
-	} else if (email2Value !== emailValue) {
-		setError(email2, "Adresy email różnią się");
+export const checkEmail2 = () => {
+	const email2Value = email2.value.trim();
+	const emailValue = email.value.trim();
+	let valid = false;
+	if (!isRequired(email2Value)) {
+		setError(email2, "Potwierdź adres e-mail");
+	} else if (emailValue !== email2Value) {
+		setError(email2, "Adresy e-mail różnią się");
 	} else {
 		setSuccess(email2);
+		valid = true;
 	}
+	return valid;
 };
 
 export const fnSavedData = function saveData() {
-	let username = document.getElementById("username").value;
-	let password = document.getElementById("password").value;
-	let email = document.getElementById("email").value;
-	let email2 = document.getElementById("email2").value;
+	let usernameValue = username.value;
+	let passwordValue = password.value;
+	let emailValue = email.value;
+	let email2Value = email2.value;
 
 	let users = [];
-	localStorage.setItem("users", JSON.stringify(users));
-	const parsedUsers = JSON.parse(localStorage.getItem("users"));
-	parsedUsers ? parsedUsers : [];
 
-	if (
-		users.some((v) => {
-			v.email === email;
-		})
-	) {
-		setError(email, "Podany email już istnieje w bazie");
-	} else {
-		const newUser = {
-			username: username,
-			password: password,
-			email: email,
-			email2: email2,
-		};
-		users = [newUser, ...users];
+	try {
+		users = JSON.parse(localStorage.getItem("users"))
+			? JSON.parse(localStorage.getItem("users"))
+			: [];
+	} catch (error) {
+		alert("SyntaxError !!!");
+	}
+
+	let newUser = {
+		username: usernameValue,
+		password: passwordValue,
+		email: emailValue,
+		email2: email2Value,
+	};
+
+	for (let user of users) {
+		console.log(user);
+		if (user.email === emailValue) {
+			setError(email, "Podany e-mail już istnieje w bazie");
+			newUser.emailValue = "";
+		}
+	}
+
+	if (newUser.emailValue !== "") {
+		users = [...users, newUser];
 
 		localStorage.setItem("users", JSON.stringify(users));
-		const user = users.find((u) => {
-			u.email === email && u.password === password;
-		});
-		localStorage.setItem("username", username);
-		localStorage.setItem("email", email);
 	}
 };
